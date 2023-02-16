9e3dfc5ad0c07d09bcefab58c684e3653bff1273
Rihanul Islam <rihanthedev@gmail.com>
2023-02-17 05:47:48 +0600

    2nd Card done

--- main.js
+++ main.js
@@ -7,24 +7,47 @@ document.getElementById('frist-card').addEventListener('click', function(){
     const productQuantity = document.getElementById('frist-quantity').innerText;
     // console.log(productName , productPrice , productQuantity);
 
+    // multiplay korchi
     const totalPrice = parseInt(productPrice) * parseInt(productQuantity);
     
     // show the data  
     displayData(productName, productPrice, productQuantity, totalPrice)
 })
 
-function displayData(productName, productPrice,productQuantity, totalPrice){
-    // ekto chek kore dekteci
-    console.log('Called');
+// using event object form borwser
+// 2nd Card
+
+document.getElementById('secound-card').addEventListener('click', function(e){
+    const pName = e.target.parentNode.parentNode.children[0].innerText;
+    const pPrice = e.target.parentNode.parentNode.children[2].children[0].innerText;
+    const pQuantity = e.target.parentNode.parentNode.children[3].children[0].innerText;
+    // console.log(productQuantity);
+    
+    // that's just  doing sum for education Purpas.although it's do to Multiplay.
+    
+    const sumTotal = parseInt(pPrice) + parseInt(pQuantity);
+    
+    // that's data give for function,whois show dislplay function.
+
+   displayData(pName,pPrice,pQuantity, sumTotal);
+})
+
+// 3rd Card
+
+
+
+// common function to display data.
+
+function displayData(pN, pP,pQ, pT){
     const container = document.getElementById('table-container');
    
     const tr = document.createElement('tr');
     tr.innerHTML = `
         <td>${1}</td>
-        <td>${productName}</td>
-        <td>${productPrice}</td>
-        <td>${productQuantity}</td>
-        <td>${totalPrice}</td>
+        <td>${pN}</td>
+        <td>${pP}</td>
+        <td>${pQ}</td>
+        <td>${pT}</td>
     `;
     container.appendChild(tr);
 }
